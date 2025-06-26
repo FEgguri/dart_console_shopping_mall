796623dbefef3ef8a0b147d6690487a82032909d
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-06-26 11:43:43 +0900

    feat:메서드 정리, 커맨드 입력 추가

--- bin/shopping_mall_dart.dart
+++ bin/shopping_mall_dart.dart
@@ -1,7 +1,9 @@
 import 'package:intl/intl.dart';
 import 'package:shopping_mall_dart/shopping_mall_dart.dart'
     as shopping_mall_dart;
+import 'dart:io';
 
+// 첫 생성 2025.06.24 이영상
 //name과 price를 가진 Product 클래스 생성 2025.06.24 이영상
 class Product {
   String name;
@@ -13,7 +15,7 @@ class Product {
 class ShoppingMall {
   List<Product> products = [];
   int totalPrice = 0;
-
+  // 생성자에서 상품 목록 초기화
   ShoppingMall()
     : products = [
         Product('Stun Gun', 2010),
@@ -25,12 +27,20 @@ class ShoppingMall {
         Product('Caramel Popcorn', 8000),
       ];
 
+  //상품 목록 출력
   void showProducts() {
-    int maxLength = 0; // 제품 이름의 최대 길이를 구하기위한 함수 2025.06.24 이영상
+    //상품목록 스타일링 시작
+    int maxLength = 0;
+    String textLine = '" * ${maxLength + 20}';
+
     for (var product in products) {
       if (maxLength < product.name.length) maxLength = product.name.length;
     }
 
+    print('[상품 목록]');
+    print('=' * (maxLength + 20));
+    print('상품명'.padRight(maxLength) + '/  가격');
+    print('"' * (maxLength + 20));
     for (var product in products) {
       //제품 이름과 가격 출력
       final formatCurrency = NumberFormat('#,###'); //원화 포맷 적용 2025.06.24 이영상
@@ -39,10 +49,41 @@ class ShoppingMall {
         '${product.name.padRight(maxLength)}  /  ${formatCurrency.format(product.price)}원',
       );
     }
+    print('"' * (maxLength + 20));
+    //상품 목록 스타일링 끝
+    //안내
   }
+
+  void addToCart() {}
+  void removeFromCart() {}
+  void showTotal() {}
 }
 
 void main(List<String> arguments) {
-  var mall = ShoppingMall();
-  mall.showProducts();
+  var shoppingMall = ShoppingMall();
+  print('[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료');
+  var inputStringValue = stdin.readLineSync();
+
+  switch (inputStringValue) {
+    case '1':
+      shoppingMall.showProducts();
+      break;
+    case '2':
+      shoppingMall.addToCart();
+      break;
+    case '3':
+      shoppingMall.showTotal();
+      break;
+    case '4':
+      print('프로그램을 종료합니다. 종료하시겠습니까? [Y/N]');
+      if (inputStringValue?.toUpperCase() == 'Y') {
+        print('프로그램을 종료합니다.');
+      } else if (inputStringValue?.toUpperCase() == 'N') {
+        print('프로그램을 계속 진행합니다.');
+        exit(0);
+      }
+
+    default:
+      print('잘못된 입력입니다. 다시 시도해주세요.');
+  }
 }
